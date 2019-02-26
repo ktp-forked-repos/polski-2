cb09c4b6a483d719376d0baa9d12948396099e54
Rodrigo Muniz <muniz95@bsd.com.br>
2019-02-26 12:54:00 +0000

    Bootstrap removed. Needs more CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-import { Nav, Navbar, NavItem } from 'react-bootstrap'
-import NavbarBrand from 'react-bootstrap/NavbarBrand'
-import NavbarCollapse from 'react-bootstrap/NavbarCollapse'
-import NavbarToggle from 'react-bootstrap/NavbarToggle'
 import { connect } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import '../styles/Header.css'
@@ -18,27 +14,18 @@ interface IState {
 class Header extends Component<IProps, IState> {
   public render () {
     return (
-      <Navbar collapseOnSelect={true}>
-        <NavbarBrand>
+      <header>
+        <div className="brand">
           <LinkContainer to={'/'}>
             <a>Polski</a>
           </LinkContainer>
-        </NavbarBrand>
-        <NavbarToggle/>
-        <NavbarCollapse>
-          <Nav>
-            {/*
-            <LinkContainer to={'/skill'}>
-              <NavItem eventKey={1}>Skill</NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/lesson'}>
-              <NavItem eventKey={2}>Lesson</NavItem>
-            </LinkContainer>
-            */}
-            <NavItem>Goal: {this.props.goal}</NavItem>
-          </Nav>
-        </NavbarCollapse>
-      </Navbar>
+        </div>
+        <div>
+          <div>
+            <div>Goal: {this.props.goal}</div>
+          </div>
+        </div>
+      </header>
     )
   }
 }
